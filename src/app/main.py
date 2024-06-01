30e01d388b63e903c63a0f3a75081d918877b915
Ana Bergese <violetagele22@gmail.com>
2024-06-01 16:17:41 +0200

    Added folder project estructure

--- src/app/main.py
+++ src/app/main.py
@@ -11,9 +11,9 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World 2"}
+    return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q, "message": "Hello World Updated"}
+    return {"item_id": item_id, "q": q, "message": q}
