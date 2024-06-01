edf6e0405af15cb50969dd834138300249261c5b
Ana Bergese <violetagele22@gmail.com>
2024-06-01 17:36:47 +0200

    fix:modify your FastAPI application to bind to 0.0.0.0

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
+```
 notifications
-├─ .venv
-├─ Dockerfile
 ├─ Docs
 ├─ README.md
 ├─ requirements.txt
 └─ src
    ├─ Docker
+   │  └─ Dockerfile.customer_request
    ├─ app
    │  ├─ __init__.py
    │  └─ main.py
    ├─ domain
+   │  ├─ __init_.py
+   │  ├─ models
+   │  │  ├─ __init_.py
+   │  │  └─ customer_request.py
+   │  └─ services
+   │     ├─ __init__.py
+   │     └─ customer_request_service.py
    ├─ entrypoints
+   │  ├─ __init__.py
+   │  └─ fastapi
+   │     ├─ __init__.py
+   │     └─ routes
+   │        └─ customer_request_route.py
    ├─ infrastructure
    └─ tests
 
--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
 from src.entrypoints.fastapi.routes import customer_request_route
+import uvicorn
 
 app = FastAPI(
     title="Customer Request Notification System",
-    description="API for handling customer requests and sending notifications to varios channels.",
+    description="API for handling customer requests and sending notifications to various channels.",
     version="1.0.0",
 )
 
@@ -13,3 +14,6 @@ app.include_router(customer_request_route.router, prefix="/api/v1/customer-reque
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Customer Request Notification System"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
