a8d690987935b399c63ede59857069fa51776255
Ana Bergese <violetagele22@gmail.com>
2024-07-01 12:21:33 +0200

    removed pycache, created Redis from main file of application_service container

--- src/application_service/entrypoints/main.py
+++ src/application_service/entrypoints/main.py
@@ -1,19 +1,54 @@
 from fastapi import FastAPI
 import uvicorn
+from redis import Redis
+from rq import Queue
+from uuid import uuid4
+from json import dumps
+from fastapi import HTTPException
 from src.application_service.entrypoints.routes import application_service_routes
+from src.application_service.entrypoints.models import RequestModel, ResponseModel
 
 app = FastAPI(
     title="Application Service",
     description="API for handling customer requests and sending notifications to various channels.",
     version="1.0.0",
 )
-
 app.include_router(application_service_routes.router, prefix="/application-service")
+
+redis_conn = Redis(host="localhost", port=6379)
+task_queue = Queue("task_queue", connection=redis_conn)
+
+def process_message(message):
+    print(f"Processing message: {message}")
+    message_data = json.loads(message)
+    print(f"Message ID: {message_data['id']}")
+    print(f"Topic: {message_data['topic']}")
+    print(f"Description: {message_data['description']}")
 
 @app.get("/")
 def read_root():
     """Server is up and running."""
     return {"message": "Application Service"}
+
+@app.post("/add-job", response_model=ResponseModel)
+async def add_job_to_queue(request: RequestModel):
+    if not request.topic or not request.description:
+        raise HTTPException(status_code=400, detail="Invalid request")
+    
+    message = {
+        "id": str(uuid4()),
+        "topic": request.topic,
+        "description": request.description
+    }
+
+    message_json = dumps(message)
+    print(f"Adding message to queue: {message_json}")  # Mensaje de depuración antes de agregar a la cola
+    task_queue.enqueue(process_message, message_json)
+    print("Message successfully added to queue")  # Mensaje de confirmación después de agregar a la cola
+
+    return ResponseModel(status=200, message="Job added to queue")
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
--- src/application_service/entrypoints/routes/application_service_routes.py
+++ src/application_service/entrypoints/routes/application_service_routes.py
@@ -1,7 +1,3 @@
-import os
-from redis import Redis
-from uuid import uuid4
-from json import dumps
 from fastapi import APIRouter, HTTPException, Depends
 from src.application_service.entrypoints.models import RequestModel, ResponseModel
 from src.application_service.domain.events import EventProvider, BotEventProvider
@@ -10,29 +6,6 @@ from src.application_service.domain.event_handlers import EventLogger, EventForw
 router = APIRouter()
 logger = EventLogger()
 forwarder = EventForwarder()
-
-redis_host = os.getenv('REDIS_HOST', 'localhost')
-redis_port = int(os.getenv('REDIS_PORT', 6379))
-redis_conn = Redis(host=redis_host, port=redis_port)
-queue_name = "notification_queue"
-
-def redis_db():
-    db = Redis(
-        host=redis_host,
-        port=redis_port,
-        db=0,
-        decode_responses=True,
-    )
-    try:
-        db.ping()
-        print("Connected to Redis")
-    except redis.ConnectionError:
-        print("Failed to connect to Redis")
-    return db
-
-def redis_queue_push(db, message):
-    db.lpush(queue_name, message)
-    print(f"Message pushed to Redis queue: {message}") 
 
 @router.post("/notify", response_model=ResponseModel)
 async def receive_request(request: RequestModel, event_provider: EventProvider = Depends(BotEventProvider)):
@@ -50,22 +23,3 @@ async def receive_request(request: RequestModel, event_provider: EventProvider =
     forwarder.forward_event(event)
 
     return ResponseModel(status=200, message="Event received, logged, and forwarded")
-
-@router.post("/add-job", response_model=ResponseModel)
-async def add_job_to_queue(request: RequestModel):
-    if not request.topic or not request.description:
-        raise HTTPException(status_code=400, detail="Invalid request")
-    
-    message = {
-        "id": str(uuid4()),
-        "topic": request.topic,
-        "description": request.description
-    }
-
-    message_json = dumps(message)
-    print(f"Adding message to queue: {message_json}")  # Mensaje de depuración antes de agregar a la cola
-    db = redis_db()
-    redis_queue_push(redis_conn, message_json)
-    print("Message successfully added to queue")  # Mensaje de confirmación después de agregar a la cola
-
-    return ResponseModel(status=200, message="Job added to queue")
