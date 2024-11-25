9b3abb22b1fd9d67e8df342e89153f5693aeca46
Ana Bergese <violetagele22@gmail.com>
2024-11-25 08:25:01 -0300

    deleted unused files

--- src/application_service/service_layer/channels.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Any
-
-
-class NotificationChannel(ABC):
-    @abstractmethod
-    async def publish(self, message: Any):
-        pass
--- src/application_service/service_layer/service.py
+++
@@ -1,9 +0,0 @@
-from .channels import NotificationChannel
-
-
-class NotificationService:
-    def __init__(self, channel: NotificationChannel):
-        self.channel = channel
-
-    async def send_notification(self, notification):
-        await self.channel.publish(notification)
