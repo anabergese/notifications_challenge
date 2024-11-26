c10acca42df5c920737595491066ed45436f3815
Ana Bergese <violetagele22@gmail.com>
2024-11-26 16:06:47 -0300

    Reordered test folder and added simple fastapi test

---
+++ tests/integration/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from src.application_service.entrypoints.routes.main import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Notification System is up and running."}
