ced2b6ddc4672a99ea65eb797675354ecc4e1aa8
Ana Bergese <violetagele22@gmail.com>
2024-11-26 09:46:31 -0300

    NotificationCreated inherits from DomainEvents

--- src/application_service/domain/events.py
+++ src/application_service/domain/events.py
@@ -1,21 +1,18 @@
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any
 
 
 @dataclass(frozen=True)
 class Event:
     timestamp: datetime
-    event_data: Any = None
 
 
 @dataclass(frozen=True)
-class DomainEvent:
-    timestamp: datetime
-    event_data: Any = None
+class DomainEvent(Event):
+    version: str = "1.0"
 
 
-@dataclass
-class NotificationCreated:
+@dataclass(frozen=True)
+class NotificationCreated(DomainEvent):
     topic: str
     description: str
