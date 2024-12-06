fbb77c62d440ba6d2746a1a39f50ff6970adcc12
Ana Bergese <violetagele22@gmail.com>
2024-12-05 21:39:32 -0300

    Added fixtures, and used python-mock instead of unittest.mock, since pytest-mock integrates unnittest and provides an automatic clean of the mocks

--- tests/integration/test_notify.py
+++ tests/integration/test_notify.py
@@ -6,43 +6,33 @@ from src.entrypoints.main import app
 client = TestClient(app)
 
 
-def test_create_notification_with_valid_topic_pricing():
-    valid_payload = {"topic": "pricing", "description": "New pricing update."}
-    response = client.post("/notify", json=valid_payload)
+def test_create_notification_with_valid_topic_pricing(valid_payload_pricing):
+    response = client.post("/notify", json=valid_payload_pricing)
     assert response.status_code == 200
     assert response.json() == {"message": "Notification created for topic: pricing"}
 
 
-def test_create_notification_with_valid_topic_sales():
-    valid_payload = {"topic": "sales", "description": "New pricing update."}
-    response = client.post("/notify", json=valid_payload)
+def test_create_notification_with_valid_topic_sales(valid_payload_sales):
+    response = client.post("/notify", json=valid_payload_sales)
     assert response.status_code == 200
     assert response.json() == {"message": "Notification created for topic: sales"}
 
 
-def test_create_notification_with_invalid_topic_string():
-    invalid_payload = {"topic": "invalid_topic", "description": "This should fail."}
+def test_create_notification_with_invalid_topics(invalid_payload):
     response = client.post("/notify", json=invalid_payload)
     assert response.status_code == 422
     assert response.json() == {"detail": "Invalid topic."}
 
 
-def test_create_notification_with_invalid_topic_number():
-    invalid_payload = {"topic": 1234, "description": "This should fail."}
-    response = client.post("/notify", json=invalid_payload)
-    assert response.status_code == 422
-    assert response.json() == {"detail": "Invalid topic."}
-
-
-def test_create_notification_with_missing_topic():
-    invalid_payload = {"description": "This should fail."}
-    response = client.post("/notify", json=invalid_payload)
+def test_create_notification_with_missing_topic(invalid_payload_missing_topic):
+    response = client.post("/notify", json=invalid_payload_missing_topic)
     assert response.status_code == 422
     assert response.json() == {"detail": "Topic is required."}
 
 
-def test_create_notification_with_missing_description():
-    invalid_payload = {"topic": "sales"}
-    response = client.post("/notify", json=invalid_payload)
+def test_create_notification_with_missing_description(
+    invalid_payload_missing_description,
+):
+    response = client.post("/notify", json=invalid_payload_missing_description)
     assert response.status_code == 422
     assert response.json() == {"detail": "Description is required."}
