9c3bdb2d260f512c585adf9f3ad341e35322fbed
Ana Bergese <violetagele22@gmail.com>
2024-12-17 17:55:33 -0300

    Build: added mypy and checked redis initialization

--- src/seedwork/application/redis_consumer.py
+++ src/seedwork/application/redis_consumer.py
@@ -1,9 +1,10 @@
 from config import get_redis_client
+from domain import enums
 
 redis_client = get_redis_client()
 
 
-async def subscribe_channel(channel):
+async def subscribe_channel(channel: enums.RedisChannels):
     pubsub = redis_client.pubsub()
     await pubsub.subscribe(channel)
     return pubsub
--- src/seedwork/application/redis_publisher.py
+++ src/seedwork/application/redis_publisher.py
@@ -2,10 +2,11 @@ import json
 import logging
 
 from config import get_redis_client
+from domain import events
 
 r = get_redis_client()
 
 
-async def publish(channel: str, event: dict):
+async def publish(channel: str, event: events.Event):
     logging.info("Publishing event...: channel=%s, event=%s", channel, event)
     await r.publish(channel, json.dumps((event)))
