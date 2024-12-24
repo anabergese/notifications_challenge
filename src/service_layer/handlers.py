dd20127ed5aa80ea78863fee9188fd4e64e6ae83
Ana Bergese <violetagele22@gmail.com>
2024-12-24 10:40:50 -0300

    Refactor: deleted non-used tests, removed redis exceptions from handlers, now catching Python exeptions. That makes the code more decouple from external service.

--- src/service_layer/handlers.py
+++ src/service_layer/handlers.py
@@ -2,10 +2,6 @@ import asyncio
 import logging
 from typing import Any, Callable, Coroutine, Dict, List, Type
 
-from redis.exceptions import BusyLoadingError
-from redis.exceptions import ConnectionError as RedisConnectionError
-from redis.exceptions import TimeoutError as RedisTimeoutError
-
 from domain.enums import RedisChannels
 from domain.events import Event, NotificationCreated, NotificationSaved
 
@@ -14,31 +10,32 @@ async def handle_notification_created(
     event: NotificationCreated,
     publish: Callable[[str, NotificationCreated], Coroutine[Any, Any, None]],
 ) -> None:
-    """Handler para procesar el evento NotificationCreated."""
 
     channel: str = RedisChannels.DB_SERVICE.value
 
     max_retries: int = 3
     for attempt in range(1, max_retries + 1):
         try:
-            await publish(channel, event)  # si hay error aqui va para DLQ
+            await publish(channel, event)
             logging.info("Event published %s: %s", channel, event)
             break
-        except (
-            RedisConnectionError,
-            RedisTimeoutError,
-            BusyLoadingError,
-        ) as redis_error:
-            logging.warning("Attempt %d failed to publish: %s", attempt, redis_error)
+        except (AttributeError, TypeError, ValueError) as data_error:
+            logging.error("Data-related error during publish: %s", data_error)
+            break
+        except (TimeoutError, OSError, ConnectionError) as infra_error:
+            logging.warning(
+                "Attempt %d failed due to infrastructure error: %s",
+                attempt,
+                infra_error,
+            )
             if attempt < max_retries:
                 await asyncio.sleep(2**attempt)  # Backoff exponencial: 2, 4, 8 segundos
             else:
-                logging.error(
-                    "Max retries reached. Failed to publish event to Redis: %s", event
-                )
+                logging.error("Max retries reached. Failed to publish event: %s", event)
         except Exception as unexpected_error:
             logging.critical(
-                "Unexpected error in Redis operation: %s", unexpected_error
+                "Unexpected error during publish operation, going to DLQ: %s",
+                unexpected_error,
             )
             break
 
