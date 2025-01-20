c0eb64e4105b30053a4937d4e8403c255a1cc2f2
Ana Bergese <violetagele22@gmail.com>
2025-01-20 07:04:31 -0300

    Refactor: deleted field from event dataclasses. Field es una funcionalidad de Pydantic y no será procesada por la dataclass estándar.I have NotificationCreated. I have NotificationRequest to make the validation of that field constraints

--- src/domain/events.py
+++ src/domain/events.py
@@ -17,12 +17,12 @@ class DomainEvent(Event):
 @dataclass(frozen=True)
 class NotificationCreated(DomainEvent):
     topic: Topic
-    description: str = Field(min_length=10, max_length=200)
+    description: str
     version: str = "1.0"
 
 
 @dataclass(frozen=True)
 class NotificationReceived(DomainEvent):
     topic: Topic
-    description: str = Field(min_length=10, max_length=200)
+    description: str
     version: str = "1.0"
