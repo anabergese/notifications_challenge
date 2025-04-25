5df146e3cda28d373a6869227d6cc82c98655d0c
Ana Bergese <violetagele22@gmail.com>
2025-04-25 10:36:53 +0200

    Refactor: changed names of enums tests

--- tests/unit/domain/test_publisher_enums.py
+++ tests/unit/domain/test_publisher_enums.py
@@ -1,23 +1,35 @@
 from enum import Enum
 
+import pytest
+
 from src.domain.publisher_enums import RedisStreams
 
 
-def test_redis_streams_available_values_are_strings():
+def test_redis_streams_are_correct_strings():
     assert RedisStreams.NOTIFICATIONS.value == "notifications"
     assert RedisStreams.NOTIFICATIONS_GROUP.value == "notifications_group"
     assert RedisStreams.NOTIFICATIONS_CONSUMER.value == "notifications_consumer"
     assert isinstance(RedisStreams.NOTIFICATIONS.value, str)
 
 
-def test_redis_message_broker_allowed_components_are_three():
+def test_redis_streams_enum_has_three_allowed_components():
     assert len(RedisStreams) == 3
 
 
-def test_redis_streams_inherit_from_enum():
+def test_redis_streams_inherit_from_enum_class():
     assert isinstance(RedisStreams.NOTIFICATIONS, Enum)
 
 
-def test_enum_comparison():
+def test_enum_comparison_works_as_expected():
     assert RedisStreams.NOTIFICATIONS == RedisStreams.NOTIFICATIONS
     assert RedisStreams.NOTIFICATIONS != RedisStreams.NOTIFICATIONS_GROUP
+
+
+def test_redis_streams_raises_value_error_for_invalid_string():
+    with pytest.raises(ValueError):
+        RedisStreams("notifications_invalid")
+
+
+def test_redis_streams_raises_value_error_for_integer_value():
+    with pytest.raises(ValueError):
+        RedisStreams(123)
--- tests/unit/domain/test_topic_enums.py
+++ tests/unit/domain/test_topic_enums.py
@@ -1,23 +1,35 @@
 from enum import Enum
 
+import pytest
+
 from src.domain.topic_enums import Topic
 
 
-def test_available_topic_values_are_strings():
+def test_topic_values_are_correct_strings():
     assert Topic.SALES.value == "sales"
     assert Topic.PRICING.value == "pricing"
     assert isinstance(Topic.SALES.value, str)
 
 
-def test_topic_allowed_values_are_two():
+def test_topic_enum_has_two_allowed_values():
     assert len(Topic) == 2
 
 
-def test_topic_inherit_from_enum():
+def test_topic_inherit_from_enum_class():
     assert isinstance(Topic.SALES, Topic)
     assert isinstance(Topic.SALES, Enum)
 
 
-def test_enum_comparison():
+def test_topic_enum_comparison_works_as_expected():
     assert Topic.SALES == Topic.SALES
     assert Topic.SALES != Topic.PRICING
+
+
+def test_topic_raises_value_error_for_invalid_string():
+    with pytest.raises(ValueError):
+        Topic("buying")
+
+
+def test_topic_raises_value_error_for_integer_value():
+    with pytest.raises(ValueError):
+        Topic(123)
